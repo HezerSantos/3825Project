c05743203f9e83e87e4f60ebb4fa132c88113176
HezerSantos <HezerSantos24@gmail.com>
2025-04-26 13:28:48 -0500

    More changes

--- client.js
+++ client.js
@@ -10,35 +10,48 @@ const options = {
 };
 
 //Create Client
-const client = tls.connect(8000, 'localhost', options, () => {
-  console.log('Connected to server \n');
-  console.log("Type --help for options\n")
-});
+try{
+  const client = tls.connect(8000, 'localhost', options, () => {
+    console.log('Connected to server \n');
+    console.log("Type --help for options\n")
+  });
+  
+  client.on('error', () => {
+    console.log("Internal Server Error")
+    process.exit();
+  })
 
-//Eventlistener for received data
-client.on('data', (data) => {
-  console.log(`${data.toString()}\n`);
-});
+  //Eventlistener for received data
+  client.on('data', (data) => {
+    console.log(`${data.toString()}\n`);
+  });
 
-//Handler for client leaving
-client.on('end', () => {
-  console.log('Disconnected from server');
-});
-
-//Handler for standard input and output
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+  //Handler for client leaving
+  client.on('end', () => {
+    console.log('Disconnected from server');
+  });
 
 
-//Method to write to the server
-rl.on('line', (input) => {
-  readline.moveCursor(process.stdout, 0, -1); // This is for clearing the input. Important for manipulating how data is sent
-  readline.clearLine(process.stdout, 0); //I am sending the data in a specific way which is why this is needed
-  client.write(input); //Sends to server
-  if (input === '--exit') {
-    client.end();
-    rl.close();
-  }
-});
+
+  //Handler for standard input and output
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+
+  //Method to write to the server
+  rl.on('line', (input) => {
+    readline.moveCursor(process.stdout, 0, -1); // This is for clearing the input. Important for manipulating how data is sent
+    readline.clearLine(process.stdout, 0); //I am sending the data in a specific way which is why this is needed
+    client.write(input); //Sends to server
+    if (input === '--exit') {
+      client.end();
+      rl.close();
+    }
+  });
+
+} catch(e){
+  console.log("Could Not connect to Server")
+}
+
